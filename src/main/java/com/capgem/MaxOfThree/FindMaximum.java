6b9404da90528a078bfde7857d2127feb45973fc
Zeeshan91490 <www.zeeshankhan91490@gmail.com>
2020-10-20 21:56:14 +0530

    src/

--- src/main/java/com/capgem/MaxOfThree/FindMaximum.java
+++ src/main/java/com/capgem/MaxOfThree/FindMaximum.java
@@ -1,36 +1,45 @@
 package com.capgemini.MaxOfThree;
 
-import java.util.Arrays;
+public class FindMaximum {
 
-public class FindMaximum<E extends Comparable<E>> {
-
-	E[] elementArray;
-
-	public FindMaximum(E[] elementArray) {
-		this.elementArray = elementArray;
+	public static Integer testMax(Integer intX, Integer intY, Integer intZ) {
+		Integer max = intX;
+		if (intY.compareTo(max) > 0) {
+			max = intY;
+		}
+		if (intZ.compareTo(max) > 0) {
+			max = intZ;
+		}
+		return max;
 	}
 
-	public E testMax() {
-		return FindMaximum.testMax(elementArray);
-	}
-
-	public static <E extends Comparable<E>> E testMax(E[] elementArray) {
-		E max = elementArray[0];
-		for (E element : elementArray)
-			if (element.compareTo(max) > 0) {
-				max = element;
-			}
-		printMax(elementArray, max);
+	public static Float testMax(Float floatX, Float floatY, Float floatZ) {
+		Float max = floatX;
+		if (floatY.compareTo(max) > 0) {
+			max = floatY;
+		}
+		if (floatZ.compareTo(max) > 0) {
+			max = floatZ;
+		}
 		return max;
 	}
 	
-	public static<E> void printMax(E[] elementArray, E max) {
-		System.out.println("Max of " + Arrays.toString(elementArray) + " is " + max );
+	public static String testMax(String stringX, String stringY, String stringZ) {
+		String max = stringX;
+		if (stringY.compareTo(max) > 0) {
+			max = stringY;
+		}
+		if (stringZ.compareTo(max) > 0) {
+			max = stringZ;
+		}
+		return max;
 	}
 
 	public static void main(String[] args) {
 
-		Integer[] arrayI = {1,2,3,4,5,6};
-		System.out.println(new FindMaximum<Integer>(arrayI).testMax());
+		System.out.println(testMax(1, 2, 3));
+		System.out.println(testMax(1.1f,1.2f,1.3f));
+		System.out.println(testMax("Apple","Peach","Banana"));
 	}
 }
+
